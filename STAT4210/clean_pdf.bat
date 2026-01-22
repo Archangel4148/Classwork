bc0a6bd92f02c604448ff778cdff4868919b1535
Simon Edmunds <sjebkc@umsystem.edu>
2026-01-22 15:15:20 -0600

    Uploading files

---
+++ STAT4210/clean_pdf.bat
@@ -0,0 +1,21 @@
+@echo off
+REM Usage: export_pdf.bat demo.ipynb
+
+if "%~1"=="" (
+    echo Usage: clean_pdf.bat notebook.ipynb
+    exit /b 1
+)
+
+set NOTEBOOK=%~1
+
+jupyter nbconvert "%NOTEBOOK%" ^
+    --to pdf ^
+    --TemplateExporter.exclude_markdown=True ^
+    --PDFExporter.latex_command=xelatex
+
+if errorlevel 1 (
+    echo Export failed.
+    exit /b 1
+)
+
+echo Export succeeded.
